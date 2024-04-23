545344caa0a9eefb51f3f2d6400fb2ce94679627
Carla <carlasantos45@hotmail.com.br>
2024-04-22 22:10:05 -0300

    Tela Regra de 3

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ texto.addEventListener("input", () => {
     let contador = (texto.value).length;
     document.getElementById("chars-quantity").textContent = `Quantidade de caracteres: ${contador}`;
 });
+
+// Regra de três
+function calcularRegraDeTres(){
+    let valor1 = document.getElementById('valor1').value;
+    let valor2 = document.getElementById('valor2').value;
+    let valor3 = document.getElementById('valor3').value;
+    let resultado = (parseFloat(valor2) * parseFloat(valor3)) / parseFloat(valor1);
+    document.getElementById('resultadoRegraDeTres').innerText = resultado;
+}
