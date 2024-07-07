c20e1f7a1ea3bb0b1233fd53725a898696accb62
Carla <carlasantos45@hotmail.com.br>
2024-07-06 21:11:53 -0300

    Ajustes segundo correção do professor

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function calcularRegraDeTres(){
     let valor2 = document.getElementById('valor2').value;
     let valor3 = document.getElementById('valor3').value;
     let resultado = (parseFloat(valor2) * parseFloat(valor3)) / parseFloat(valor1);
-    document.getElementById('resultadoRegraDeTres').innerText = resultado;
+    let resultadoFormatado = resultado.toFixed(2);
+    resultadoFormatado = resultadoFormatado.replace('.', ',');
+    document.getElementById('resultadoRegraDeTres').innerText = resultadoFormatado;
 }
 
 //Acrescimo
@@ -19,12 +21,16 @@ function calcularAcrescimoPorcentagem(){
     let valorInicial = document.getElementById('valorInicialAcrescimo').value;
     let valorAcrescimo = document.getElementById('valorAcrescimo').value;
     let resultado = parseFloat(valorInicial) + (parseFloat(valorAcrescimo)*(parseFloat(valorInicial)/100));
-    document.getElementById('resultadoAcrescimo').innerText = resultado;
+    let resultadoFormatado = resultado.toFixed(2);
+    resultadoFormatado = resultadoFormatado.replace('.', ',');
+    document.getElementById('resultadoAcrescimo').innerText = resultadoFormatado;
 }
 //Desconto
 function calcularDescontoPorcentagem(){
     let valorInicial = document.getElementById('valorInicialDesconto').value;
     let valorDesconto = document.getElementById('valorDesconto').value;
     let resultado = parseFloat(valorInicial) - (parseFloat(valorDesconto)*(parseFloat(valorInicial)/100));
-    document.getElementById('resultadoDesconto').innerText = resultado;
+    let resultadoFormatado = resultado.toFixed(2);
+    resultadoFormatado = resultadoFormatado.replace('.', ',');
+    document.getElementById('resultadoDesconto').innerText = resultadoFormatado;
 }
